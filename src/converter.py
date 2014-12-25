3608cfe893e1f6c127315665f5d1249134573c60
RaiaN <raianknight@gmail.com>
2014-12-26 00:08:16 +0400

    Add basic converter interface

---
+++ src/converter.py
@@ -0,0 +1,26 @@
+class Converter:
+    def __init__(self, scheme_filename):
+        with open(self.scheme_filename) as sf:
+            self.scheme = sf.readlines()
+            
+                
+    def convert(self):
+        scheme_converted_str = ""
+    
+        return scheme_converted_str
+      
+      
+    def get_description(self):
+        pass
+    
+    
+    def get_scheme_name(self):
+        pass
+    
+    
+    def parse_elements(self):
+        pass
+    
+    
+    def build_workflow(self):
+        pass     
