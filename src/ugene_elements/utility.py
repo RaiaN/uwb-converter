24e50813476aa960c4caaeb6b0cafc9edf2ea827
RaiaN <raianknight@gmail.com>
2014-12-27 01:22:23 +0400

    Add utility module

---
+++ src/ugene_elements/utility.py
@@ -0,0 +1,22 @@
+def prepare_datasets(datasets):   
+    good_datasets = []
+    
+    for dataset in datasets:
+        name = dataset[0].split('"')[1]
+        
+        files = []
+        dirs  = []
+        
+        if len(dataset) > 1:
+            for line in dataset[1:]:
+                ftype, fpath = line.strip()[:-1].split(':') #remove semicolon at the end 
+                if ftype == "file":
+                    files.append(fpath)
+                elif ftype == "dir":
+                    dirs.append(fpath)
+                else:
+                    raise Exception("Not supported type of file")
+                
+        good_datasets.append([name, files, dirs]) 
+        
+    return good_datasets               
