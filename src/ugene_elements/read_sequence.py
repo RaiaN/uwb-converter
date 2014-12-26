b9e5017520864e343402e79a8eed08a5987ede9a
RaiaN <raianknight@gmail.com>
2014-12-27 02:53:13 +0400

    Add read sequence

---
+++ src/ugene_elements/read_sequence.py
@@ -0,0 +1,37 @@
+import utility 
+
+
+class ReadSequence:
+    def __init__(self, name, datasets):
+        self.name     = name
+        self.datasets = datasets
+        self.imports  = []
+        
+    def generate_code(self):
+        datasets = utility.prepare_datasets(self.datasets)
+        
+        code = []
+        
+        line = 'seq_records = []'
+        code.append(line)
+        
+        for dataset in datasets:
+            files = dataset[1] + utility.files_from_dirs(dataset[2])
+                        
+            if len(files) > 0:
+                line = 'for filename in [%s]:' % (",".join(files)) 
+                code.append(line)
+                
+                line = 'ftype = filename.split(".")[-1]'
+                code.append(line)
+                
+                line = 'seq_records += [rec for rec in SeqIO.parse(filename, ftype):'
+                code.append(line)
+                
+                utility.add_end_indentation_line(code)
+        
+        self.imports.append("from Bio import SeqIO")
+        
+        self.code = code
+        
+                    
