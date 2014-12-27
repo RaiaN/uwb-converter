4d0523264c4cbe2715db5cae6674a7fca0893640
RaiaN <raianknight@gmail.com>
2014-12-27 17:57:47 +0400

    Add some writers

--- src/ugene_elements/write_text.py
+++ src/ugene_elements/write_text.py
@@ -1,7 +1,7 @@
 import utility
 
 
-class WriteFasta:
+class WriteText:
     def __init__(self, name, input_data, output_path, elem_id):
         self.name = name        
         self.input_data = input_data
@@ -23,12 +23,13 @@ class WriteFasta:
         line = 'with open(%s, "w") as outp_f:' % (self.output_dir + self.output_path)
         code.append(line)
         
-        line = 'SeqIO.write(%s, outp_f, "fasta")' % (self.input_data) 
+        line = 'for line in %s' % self.input_data
         code.append(line)
         
-        utility.add_end_indentation_line(code)
-        utility.add_empty_line(code)
+        line = 'outp_f.write(line + "\n")'
+        code.append(line)
         
-        self.imports.append("from Bio import SeqIO")
+        utility.add_end_indentation_line(code, count=2)
+        utility.add_empty_line(code)
                        
         self.code = code    
