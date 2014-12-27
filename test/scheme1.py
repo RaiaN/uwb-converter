5fef6dd4303c051f01018eb62d3ef47de41bb33d
RaiaN <raianknight@gmail.com>
2014-12-27 17:20:59 +0400

    Add missing generated output

---
+++ test/scheme1.py
@@ -0,0 +1,24 @@
+from Bio import Entrez
+from Bio import SeqIO
+
+# Загрузить из удаленной базы данных
+Entrez.email = "Put your email here (for Entrez)"
+handle = Entrez.efetch(db="nuccore", id="ewqe", rettype="gb", retmode="text")
+seq_record0 = SeqIO.read(handle, "gb")
+
+
+records2 = []
+# Чтение аннотаций Dataset 1
+for filename in ["/home/raian/eqwe.uwl","/home/raian/nohup.out","/home/raian/System Report.txt"]:
+    ftype = filename.split(".")[-1]
+    records2 += [rec for rec in SeqIO.parse(filename, ftype)]
+
+# Чтение аннотаций Dataset 2
+for filename in ["/home/raian/algo-hw-4.pdf"]:
+    ftype = filename.split(".")[-1]
+    records2 += [rec for rec in SeqIO.parse(filename, ftype)]
+
+
+# Список файлов 1
+datasets7 = [["Dataset 1", ["/home/raian/liclipse/workspace/bio_project/test/scheme1.uwl","/home/raian/liclipse/workspace/bio_project/test/scheme2.uwl"]]]
+
