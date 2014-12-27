1ec482cbec2c1c7052f1f5c4cd673142eea40a03
RaiaN <raianknight@gmail.com>
2014-12-27 22:07:21 +0400

    Add convert alignment to sequence

---
+++ src/ugene_elements/convert_alignment_to_sequence.py
@@ -0,0 +1,40 @@
+import utility
+
+
+class ConvertAlignmentToSequence:
+    def __init__(self, name, elem_id, true_elem_name):
+        self.name = name        
+        self.input_data = None
+        self.output = None
+        self.elem_id = elem_id
+        self.imports = []
+        self.true_elem_name = true_elem_name
+        
+        
+    def generate_code(self):
+        code = []
+        
+        if self.input_data is None:
+            self.code = code
+            return
+        
+        line = '# ' + self.name
+        code.append(line)
+        
+        self.output = "seqs" + str(self.elem_id)           
+        
+        line = '%s = []' % self.output 
+        code.append(line)
+        
+        line = 'for record in %s:' % self.input_data
+        code.append(line)
+        
+        line = '%s.append(record.seq)' % self.output
+        code.append(line)
+                       
+        utility.add_end_indentation_line(code)
+        utility.add_empty_line(code)
+        
+        self.imports.append("from Bio import AlignIO")
+                       
+        self.code = code    
--- test/scheme1.py
+++ test/scheme1.py
@@ -1,14 +1,18 @@
-from Bio import SeqIO
+from Bio import AlignIO
 
-seq_records1 = []
-# Чтение последовательности Dataset 1
-for filename in ["/home/raian/ugene-1.14.2/data/samples/FASTA/human_T1.fa"]:
+records1 = []
+# Чтение множественного выравнивания Dataset 1
+for filename in ["/home/raian/ugene-1.14.2/data/samples/CLUSTALW/COI.aln"]:
     ftype = filename.split(".")[-1]
-    seq_records1 += [rec for rec in SeqIO.parse(filename, ftype)]
+    records1 += [rec for rec in AlignIO.parse(filename, ftype)]
 
 
-# Обратная комплементарность
-reverse_complements0 = []
-for record in seq_records1:
-    reverse_complements0.append(record.seq.reverse_complement())
+# Разделение выравнивание на последовательности
+seqs0 = []
+for record in records1:
+    seqs0.append(record.seq)
+
+# Запись множественного выравнивания
+with open("biopython_data/outp", "w") as outp_f:
+    AlignIO.write(seqs0, outp_f, "clustal")
 
