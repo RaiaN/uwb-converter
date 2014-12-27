22debb8bd84fd1b19a497aed9c3ee53aefb88bd2
RaiaN <raianknight@gmail.com>
2014-12-27 18:14:09 +0400

    Add write variations

---
+++ src/ugene_elements/write_variations.py
@@ -0,0 +1,42 @@
+import utility
+
+
+class WriteSequence:
+    def __init__(self, name, input_data, output_path, elem_id):
+        self.name = name        
+        self.input_data = input_data
+        self.output_path = output_path
+        self.elem_id = elem_id
+        self.output_dir = "biopython_data/"
+        self.imports = []
+        
+    def generate_code(self):
+        code = []
+        
+        if self.input_data is None:
+            self.code = code
+            return
+        
+        line = '# ' + self.name
+        code.append(line)
+        
+        line = 'with open(%s, "w") as outp_f:' % (self.output_dir + self.output_path)
+        code.append(line)
+        
+        line = 'for line in %s:' % self.input_data
+        code.append(line)
+        
+        line = 'outp_f.write(line + "\n")' % (self.input_data) 
+        code.append(line)
+        
+        utility.add_end_indentation_line(code)
+        
+        line = 'outp_f.write("\n")'
+        code.append(line)
+        
+        utility.add_end_indentation_line(code)
+        utility.add_empty_line(code)
+        
+        self.imports.append("from Bio import SeqIO")
+                       
+        self.code = code    
